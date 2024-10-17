45f60f822834f3923c569737ecb39640b7f90722
natscott1738 <natscott383@gmail.com>
2024-10-17 15:16:48 +0300

    python async comprehension

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -8,6 +8,7 @@ from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
+
 async def async_comprehension() -> List[float]:
     """ Async Comprehension """
     a = [i async for i in async_generator()]
--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,6 +7,7 @@ from time import time
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     """ Run time for four parallel comprehensions """
     start = time()
